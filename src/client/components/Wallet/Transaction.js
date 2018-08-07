793c393d6ec15b57c502818c1cd480a3fdddf41b
Mitchzof <mitchellchase99@gmail.com>
2018-08-07 15:45:47 +0700

    Fixed testnet transaction link bug

--- src/client/components/Wallet/Transaction.js
+++ src/client/components/Wallet/Transaction.js
@@ -37,7 +37,7 @@ class Transaction extends Component {
         <div style={{ width: '20%', minWidth: '100px' }}><p>{ (this.props.tx.isRefund) ? 'Debt Settlement' : (this.props.tx.type == 'payment') ? 'Direct Payment' : 'Path Payment' }</p></div>
         <div style={{ width: '20%', minWidth: '100px' }}><p className="selectable" style={{ width: '100px' }} onClick={ this.copyToClipboard }>{ this.props.tx.from.slice(0, 5) }...{ this.props.tx.from.slice(this.props.tx.from.length - 5, this.props.tx.from.length) }</p></div>
         <div style={{ width: '20%', minWidth: '100px' }}><p>{ this.date.toDateString() }</p></div>
-        <div style={{ width: '5%', minWidth: '25px' }}><a href={ "https://stellar.expert/explorer/public/tx/" + this.props.tx.transaction_hash } target='_blank'><i className="material-icons tiny">call_made</i></a></div>
+        <div style={{ width: '5%', minWidth: '15px' }}><a href={ "https://stellar.expert/explorer/" + (TESTNET) ? 'testnet' : 'public' + "/tx/" + this.props.tx.transaction_hash } target='_blank'><i className="material-icons tiny">call_made</i></a></div>
         <div style={{ width: '2%', minWidth: '20px' }}>{ (this.props.tx.memo) ?
           <a className="dropdown" style={{color: '#2c662d' }} onClick={ this.expand }>
             {
@@ -58,7 +58,7 @@ class Transaction extends Component {
         <div style={{ width: '20%', minWidth: '100px' }}><p>{ (this.props.tx.isRefund) ? 'Debt Settlement' : (this.props.tx.type == 'payment') ? 'Direct Payment' : 'Path Payment' }</p></div>
         <div style={{ width: '20%', minWidth: '100px' }}><p className="selectable" onClick={ this.copyToClipboard } style={{ width: '100px' }}>{ this.props.tx.to.slice(0, 5) }...{ this.props.tx.to.slice(this.props.tx.to.length - 5, this.props.tx.to.length) }</p></div>
         <div style={{ width: '20%', minWidth: '100px' }}><p>{ this.date.toDateString() }</p></div>
-        <div style={{ width: '5%', minWidth: '15px' }}><a href={ "https://stellar.expert/explorer/testnet/tx/" + this.props.tx.transaction_hash } target='_blank'><i className="material-icons tiny">call_made</i></a></div>
+        <div style={{ width: '5%', minWidth: '15px' }}><a href={ "https://stellar.expert/explorer/" + (TESTNET) ? 'testnet' : 'public' + "/tx/" + this.props.tx.transaction_hash } target='_blank'><i className="material-icons tiny">call_made</i></a></div>
         <div style={{ width: '2%', minWidth: '10px' }}>{ (this.props.tx.memo) ?
           <a className="dropdown" style={{color: '#9f3a38' }} onClick={ this.expand }>
             {
